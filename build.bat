ab9b96353f7a5ea437593c7161b0721fa7b72078
Kelly Selden <kellyselden@gmail.com>
2015-04-09 16:21:08 -0400

    rebuild on "Cleanup error"

--- build.bat
+++ build.bat
@@ -1,11 +1,27 @@
 pushd %~dp0
+set count=0
+:restart-build
 call clean-tmp
 call install
 call version
 pushd ..
 pushd client
 <nul set /p=%2>host
-call ember build --environment=%1
+call ember build --environment=%1 >stdout.txt
+type stdout.txt
+findstr /C:"Cleanup error" stdout.txt
+if %errorlevel%==0 goto :error
+goto :success
+:error
+set /a count+=1
+if %count% lss 10 (
+  popd
+  goto :restart-build
+) else (
+  exit /b 1
+)
+:success
+del stdout.txt
 del /q host
 popd
 if exist server\public (rd server\public /s /q)
