7758bf2b1601f67aa355ba71ac717a1d606a1b73
Kelly Selden <kellyselden@gmail.com>
2015-06-15 23:07:11 -0400

    can't deploy here anymore

--- deploy-heroku.bat
+++ deploy-heroku.bat
@@ -1,9 +1,9 @@
 pushd %~dp0
 for /f %%i in (heroku) do set heroku=%%i
 call build heroku
-pushd ..\server
+pushd ..
 md heroku
-ROBOCOPY /MIR . heroku /XD node_modules heroku /XF .gitignore
+ROBOCOPY /MIR server heroku /XD node_modules heroku /XF .git /XF .gitignore
 pushd heroku
 git init
 git add . && git commit -a -m "deploy"
